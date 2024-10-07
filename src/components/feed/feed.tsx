970205c858c90a977abb289ceb0df7c7e3c13362
tilo1306 <doug1306@gmail.com>
2024-10-07 16:21:03 -0300

    feat: Feed

---
+++ src/components/feed/feed.tsx
@@ -0,0 +1,8 @@
+import { Photo } from '@/actions/photos-get';
+import FeedPhotos from './feed-photos';
+
+export default function Feed({ photos }: { photos: Photo[] }) {
+    return <div>
+        <FeedPhotos photos={photos} />
+    </div>
+}
