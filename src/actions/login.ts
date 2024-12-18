606eaffb1c7f456b2e4630a4621f01a192838bea
tilo1306 <doug1306@gmail.com>
2024-12-18 19:48:16 -0300

    feat: crair usuario

--- src/actions/login.ts
+++ src/actions/login.ts
@@ -1,42 +1,30 @@
-'use server'
+'use server';
 
-import { TOKEN_POST } from '@/functions/api'
-import apiError from '@/functions/api-error'
-import { cookies } from 'next/headers'
+import { TOKEN_POST } from '@/functions/api';
+import apiError from '@/functions/api-error';
+import { cookies } from 'next/headers';
 
 export default async function login(state: {}, formData: FormData) {
-    const username = formData.get('username') as string | null
-    const password = formData.get('password') as string | null
+  const username = formData.get('username') as string | null;
+  const password = formData.get('password') as string | null;
 
-    const { url } = TOKEN_POST()
-
-    try {
-
-        if (!username || !password) throw new Error('Preencha od dados.')
-
-        const response = await fetch(url,
-            {
-                method: 'POST',
-                body: formData,
-            }
-        )
-
-        const data = await response.json()
-
-        if (!response.ok) throw new Error('Senha ou usuário inválido')
-
-        cookies().set('token', data.token, {
-            httpOnly: true,
-            secure: true,
-            sameSite: 'lax',
-            maxAge: 60 * 60 * 24
-        })
-
-        return { data: null, ok: true, error: '' }
-
-    } catch (error: unknown) {
-
-        return apiError(error)
-
-    }
+  try {
+    if (!username || !password) throw new Error('Preencha os dados.');
+    const { url } = TOKEN_POST();
+    const response = await fetch(url, {
+      method: 'POST',
+      body: formData,
+    });
+    if (!response.ok) throw new Error('Senha ou usuário inválidos.');
+    const data = await response.json();
+    cookies().set('token', data.token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'lax',
+      maxAge: 60 * 60 * 24,
+    });
+    return { data: null, ok: true, error: '' };
+  } catch (error: unknown) {
+    return apiError(error);
+  }
 }
--- src/app/login/criar/page.tsx
+++ src/app/login/criar/page.tsx
@@ -1,5 +1,16 @@
+import LoginCriarForm from '@/components/login/login-criar-form';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Crie sua conta',
+  description: 'Crie sua conta no site Dogs.',
+};
+
 export default async function CriarPage() {
-    return <main>
-        <h1>udhashusduhsd</h1>
-    </main>
+  return (
+    <div className="animeLeft">
+      <h1 className="title">Cadastre-se</h1>
+      <LoginCriarForm />
+    </div>
+  );
 }
