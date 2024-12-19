3911969046c6936d2d4f80d5ae4d59dad9f9462f
tilo1306 <doug1306@gmail.com>
2024-12-18 21:45:23 -0300

    feat: reset password

--- src/app/login/resetar/page.tsx
+++ src/app/login/resetar/page.tsx
@@ -1,5 +1,28 @@
-export default async function ResetarPage() {
-    return <main>
-        <h1>udhashusduhsd</h1>
-    </main>
+import LoginResetarForm from '@/components/login/login-resetar-form';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Resetar a senha | Dogs',
+  description: 'Resete a sua senha',
+};
+
+type ResetarSearchParams = {
+  searchParams: {
+    key: string;
+    login: string;
+  };
+};
+
+export default async function ResetarPage({
+  searchParams,
+}: ResetarSearchParams) {
+  return (
+    <div className="animeLeft">
+      <h1 className="title">Resete a Senha</h1>
+      <LoginResetarForm
+        keyToken={searchParams.key}
+        login={searchParams.login}
+      />
+    </div>
+  );
 }
