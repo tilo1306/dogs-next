6ad913bfa362ad44c45e76ffe8376b311ae05fd8
tilo1306 <doug1306@gmail.com>
2024-12-18 22:21:15 -0300

    feat: creat context

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,28 +3,34 @@ import './globals.css';
 import { type_second } from '@/functions/fonts';
 import Header from '@/components/header';
 import Footer from '@/components/footer';
+import { UserContextProvider } from '@/context/user-context';
+import userGet from '@/actions/user-get';
 
 export const metadata: Metadata = {
   title: 'Dogs Next',
   description: 'Rede social para cachorros.',
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
+  modal,
 }: Readonly<{
   children: React.ReactNode;
+  modal: React.ReactNode;
 }>) {
+  const { data: user } = await userGet();
+
   return (
     <html lang="pt-BR">
       <body className={type_second.variable}>
-        <div className='App'>
-          <Header />
-          <main className='AppBody'>
-            {children}
-          </main>
-          <Footer />
-        </div>
-
+        <UserContextProvider user={user}>
+          <div className="App">
+            <Header />
+            <main className="AppBody">{children}</main>
+            <div>{modal}</div>
+            <Footer />
+          </div>
+        </UserContextProvider>
       </body>
     </html>
   );
